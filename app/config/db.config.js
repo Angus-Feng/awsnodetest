ef0a67e6f761215d86afb617523112e0f5d9777c
Feng <85354711+Angus-Feng@users.noreply.github.com>
2021-11-23 11:44:55 -0500

    bbb

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    PORT: "3333",
-    USER: "wsday12todosnodejs",
-    PASSWORD: "1WFanGMs!s-Bz-4R",
-    DB: "wsday12todosnodejs"
+    // HOST: "localhost",
+    // PORT: "3333",
+    // USER: "wsday12todosnodejs",
+    // PASSWORD: "1WFanGMs!s-Bz-4R",
+    // DB: "wsday12todosnodejs"
   };
